0f517c3815638487d3d77f243bf397dd6beedc46
aidemar <aidemarmag@hotmail.com>
2018-01-21 22:28:33 -0600

    intento de funcionalidad

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var data = [
+	{
+		'name': 'La biela',
+		'kind': 'Argentina',
+		'location': 'Cto Puericultores 42, Cd. Satélite, 53100 Naucalpan de Juárez, Méx.'
+		
+
+	},
+		{
+
+		'name': 'Italiannis',
+		'kind': 'Italiana',
+		'location': 'Perif. Blvd. Manuel Ávila Camacho 1007, Hab Jardines de Santa Monica, 54050 Tlalnepantla, Méx.'
+	},
+
+		{
+		'name': 'Romano´s Macaroni Grill',
+		'kind': 'Italiana',
+		'location': 'Cto Centro Comercial 2251, Cd. Satélite, 53100 Naucalpan de Juárez, Méx.'
+	},
+
+		{
+		'name': 'Panda Express',
+		'kind': 'China',
+		'location': 'Cto Centro Comercial 2251, Cd. Satélite, 53100 Naucalpan de Juárez, Méx.'
+	},
+
+		{
+		'name': 'Brasileiríssimo',
+		'kind': 'Brasileña',
+		'location': 'Circuito Cirujanos 16, Cd. Satélite, 53100 Naucalpan de Juárez, Méx.'
+	}
+
+
+
+	];
+
